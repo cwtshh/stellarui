e79aa5284f91adc26eb4e1e2ed2d5a4aa1c9a38e
JaumC <jvvianachaves@gmail.com>
2024-10-15 10:35:24 -0300

    chat textarea + scrow chat down

--- src/components/ChatBubble/UserChatBubble.tsx
+++ src/components/ChatBubble/UserChatBubble.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { MessageType } from '../../utils/types/ChatType'
-import { useAuth } from '../../context/AuthContext'
 
 interface UserChatBubbleProps {
     message: MessageType
@@ -8,22 +7,11 @@ interface UserChatBubbleProps {
 
 const UserChatBubble = ( {message} : UserChatBubbleProps) => {
 
-    const { user } = useAuth();
-    const first_name = user?.name.split(' ')[0];
     const date = new Date(message.created_at).toLocaleString('pt-br');
     return (
         <div className="chat chat-end">
-            <div className="chat-image avatar">
-                <div className="w-10 rounded-full">
-                <img
-                    alt="Tailwind CSS chat bubble component"
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                </div>
-            </div>
-            <div className="chat-header text-white">
-                {first_name}
-            </div>
-            <div className="chat-bubble bg-secondary">{message.content}</div>
+            <div className="chat-image avatar"></div>
+            <div className="chat-bubble bg-secondary break-words whitespace-pre-wrap">{message.content}</div>
             <div className="chat-footer text-white opacity-50">{date}</div>
         </div>
     )
--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef } from 'react'
-import { useChat } from '../../context/ChatContext'
+import React, { useState, useRef, useEffect } from 'react';
+import { useChat } from '../../context/ChatContext';
 import { BsSendFill } from 'react-icons/bs';
 import { IoMdAttach } from 'react-icons/io';
 import UserChatBubble from '../../components/ChatBubble/UserChatBubble';
@@ -8,27 +8,43 @@ import AssistantChatBubble from '../../components/ChatBubble/AssistantChatBubble
 
 const Chat = () => {
   const { selectedChat, send_message } = useChat();
-  const [ model, setModel ] = useState('Gemma 2');
-  const [ message, setMessage ] = useState('');
+  const [model, setModel] = useState('Gemma 2');
+  const [message, setMessage] = useState('');
   const chat_date = new Date(selectedChat?.created_at ?? '').toLocaleString('pt-br');
 
   const fileInputRef = useRef(null);
+  const messagesEndRef = useRef(null);
+  const textareaRef = useRef(null);
 
   const handleIconClick = () => {
-      fileInputRef.current.click();
+    fileInputRef.current.click();
   };
-  
+
   const handleMessage = (e: React.FormEvent) => {
     e.preventDefault();
     if (message.trim()) {
       send_message(message);
       setMessage('');
     }
-  }
+  };
+
+  // Efeito para rolar automaticamente para o fim quando novas mensagens são adicionadas
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [selectedChat?.messages]);
+
+  // Função para ajustar a altura do textarea dinamicamente
+  const adjustTextareaHeight = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto'; // Reseta a altura
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`; // Ajusta a altura ao conteúdo
+    }
+  };
 
   return (
     <div className='h-full w-full flex flex-col justify-between'>
-      
       <div className='flex justify-between text-xl bg-primary w-full p-4'>
         <details className="dropdown">
           <summary className='cursor-pointer text-white'>Model {model}</summary>
@@ -39,54 +55,60 @@ const Chat = () => {
         </details>
         {selectedChat && (
           <p className='text-white'>{chat_date}</p>
-        )}  
+        )}
       </div>
 
-      <div className='overflow-y-scroll flex flex-col w-full h-full p-6' style={{backgroundImage: `url(${chatbg})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat"}}>
-        
+      <div className='overflow-y-scroll flex flex-col w-full h-full p-6' style={{ backgroundImage: `url(${chatbg})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
         {selectedChat ? selectedChat?.messages.map((message, index) => (
           message.sent_by === 'user' ? (
-        <UserChatBubble message={message} key={index} />
+            <UserChatBubble message={message} key={index} />
           ) : (
-        <AssistantChatBubble message={message} key={index} />
+            <AssistantChatBubble message={message} key={index} />
           )
         )) : (
           <div className='flex'>
-        <div>
-          <h1 className='text-2xl text-white'>Selecione um chat para começar a conversar</h1>
-        </div>
+            <div>
+              <h1 className='text-2xl text-white'>Selecione um chat para começar a conversar</h1>
+            </div>
           </div>
         )}
 
+        {/* Div invisível que marca o fim da lista de mensagens */}
+        <div ref={messagesEndRef} />
       </div>
 
-      <div className='w-full p-3'>
-        {/* <label className="input input-bordered flex items-center gap-2">
-          <IoMdAttach className='text-2xl' role='input' type='file' />
-          <input type="text" className="grow" placeholder="Pergunte o que quiser..." />
-          <BsSendFill />
-        </label> */}
+      <div className='w-full max-h-[250px] p-3'>
         <div className='flex items-center gap-2'>
           <div>
-              <IoMdAttach 
-                  className='text-2xl cursor-pointer' 
-                  onClick={handleIconClick} // Chama a função ao clicar no ícone
-              />
-              <input 
-                  type='file' 
-                  ref={fileInputRef} // Define a referência ao input
-                  className='hidden' // Oculta o input de arquivo
-                  onChange={(e) => {
-                      // Manipulador para quando um arquivo for selecionado
-                      const file = e.target.files[0];
-                      if (file) {
-                          console.log('Arquivo selecionado:', file);
-                      }
-                  }} 
-              />
+            <IoMdAttach
+              className='text-2xl cursor-pointer'
+              onClick={handleIconClick}
+            />
+            <input
+              type='file'
+              ref={fileInputRef}
+              className='hidden'
+              onChange={(e) => {
+                const file = e.target.files[0];
+                if (file) {
+                  console.log('Arquivo selecionado:', file);
+                }
+              }}
+            />
           </div>
-          <form className='flex w-full items-center' onSubmit={handleMessage}>
-            <input onChange={e => setMessage(e.target.value)} type="text" value={message} className="input input-bordered w-full" placeholder="Pergunte o que quiser..." />
+          <form className='flex w-full h-full items-center break-words whitespace-pre-wrap' onSubmit={handleMessage}>
+            <textarea
+              ref={textareaRef}
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value);
+                adjustTextareaHeight();
+              }}
+              className="textarea textarea-bordered w-full max-h-[200px] overflow-auto resize-none"
+              placeholder="Pergunte o que quiser..."
+              rows={1} // Começa com uma linha
+              style={{ height: 'auto' }} // Estilo inicial para altura
+            />
             <button className='btn ml-2 h-full'>
               <BsSendFill className='text-2xl' />
             </button>
@@ -94,7 +116,7 @@ const Chat = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
