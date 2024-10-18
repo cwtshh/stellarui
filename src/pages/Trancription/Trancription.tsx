d56c4c2fee3c227f983905acfcd7324326d3aa8c
JaumC <jvvianachaves@gmail.com>
2024-10-18 09:41:37 -0300

    file input added

--- src/pages/Trancription/Trancription.tsx
+++ src/pages/Trancription/Trancription.tsx
@@ -3,7 +3,7 @@ import { FaDownload } from 'react-icons/fa';
 import { TranscriptionCard } from '../../components/TranscriptionCard/TranscriptionCard';
 import { NotifyToast } from '../../components/Toast/Toast';
 import axios from 'axios';
-// Importar o TranscriptionCard e ding_ding conforme necessário
+import chatbg from '../../assets/chatbg.jpeg';
 
 interface SegmentsBody {
   id: number;
@@ -11,13 +11,13 @@ interface SegmentsBody {
   start: number;
   end: number;
   text: string;
-
 }
 
 interface ResultBody {
   text: string;
   segments: SegmentsBody[];
 }
+
 interface ResponseBody {
   filename: string;
   conversion_time: number;
@@ -28,7 +28,7 @@ interface ResponseBody {
 const Trancription = () => {
   const [file, setFile] = useState<File | null>(null);
   const [videoUrl, setVideoUrl] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(false); // Estado para controlar o carregamento
+  const [loading, setLoading] = useState<boolean>(false);
   const [ segments, setSegments ] = useState<any[]>([]);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,7 +43,6 @@ const Trancription = () => {
       setLoading(true);
       const formData = new FormData();
       formData.append('file', file);
-      console.log(formData);
       
       try {
         const response: any = await axios.post('http://0.0.0.0:8045/upload-video/', formData);
@@ -80,60 +79,59 @@ const Trancription = () => {
     }
   }, [file]);
 
-  
-
   return (
-    <div className='h-full w-full flex flex-col p-5'>
-      {file ? (
-        <>
-          <div className='flex items-start justify-center h-full gap-[95px]'>
-            <div className='text-white p-6 h-vh w-[30%] rounded-xl flex flex-col gap-7'>
-              <div className='bg-primary p-6 scroll-hidden h-[800px] w-[500px] rounded-xl flex flex-col gap-6 overflow-y-scroll shadow-xl'>
-                {segments.map((item: any, index: number) => {
-                  return (
-                    <TranscriptionCard key={index} text={item.text} speaker='Speaker' />
-                  )
-                })}
-              </div>
-              <div className='flex justify-center items-center w-[500px]'>
-                <button className='btn btn-primary w-[300px] flex justify-center items-center text-white p-6 rounded-xl h-full'>
-                  Transcrição
-                  <FaDownload />
-                </button>
-              </div>
-            </div>
-            <div className='flex flex-col gap-5 w-[900px] h-[700px] items-center justify-center p-6'>
-              <p>{file.name}</p>
-              {loading ? (
-                <div className="flex items-center justify-center h-64">
-                  <progress className="progress w-56"></progress>
-                </div>
-              ) : (
-                videoUrl && (
-                  <video 
-                    src={videoUrl} 
-                    className='rounded-xl shadow-xl w-[100%] h-[100%]' 
-                    controls 
-                    preload="false" // Carregamento antecipado
-                    muted 
-                  />
-                )
-              )}
-            </div>
+    <div className='h-full w-full flex flex-col p-5' style={{ backgroundImage: `url(${chatbg})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+      <div className='flex items-start justify-center h-full gap-[95px]'>
+        <div className='text-white p-6 h-vh w-[30%] rounded-xl flex flex-col gap-7'>
+          <div className='bg-base-100 p-6 scroll-hidden h-[800px] w-[500px] rounded-xl flex flex-col gap-6 overflow-y-scroll shadow-xl'>
+            {segments.map((item: any, index: number) => {
+              return (
+                <TranscriptionCard key={index} text={item.text} speaker='Speaker' />
+              )
+            })}
           </div>
-        </>
-      ) : (
-        <>
-          <div>
-            <input 
-              type="file" 
-              onChange={handleFileChange} 
-              className="file-input file-input-bordered w-full max-w-xs" 
+          <div className='flex justify-center items-center w-[500px]'>
+            <button className='btn btn-primary w-[300px] flex justify-center items-center text-white p-6 rounded-xl h-full'>
+              Transcrição
+              <FaDownload />
+            </button>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-5 w-[900px] h-[800px] items-center justify-center p-6'>
+          {!file && (
+            <div className='bg-transparent border-[2px] w-[335px] h-[60px] flex justify-center items-center rounded-xl'>
+              <input
+                type="file"
+                onChange={handleFileChange}
+                className="file-input file-input-bordered w-full max-w-xs"
+              />
+            </div>
+          )}
+
+          {loading && (
+            <>
+              <div className="flex items-center flex-col justify-center h-64">
+                <p className='text-white'>{file?.name}</p>
+                <progress className="progress w-56 bg-secondary"></progress>
+              </div>
+            </>
+          )}
+
+          {videoUrl && !loading && (
+            <video
+              src={videoUrl}
+              className='rounded-xl shadow-xl'
+              style={{ width: '100%', height: 'auto', maxHeight: '100%' }}
+              controls
+              preload="false"
+              muted
             />
-          </div>
-        </>
-      )}
+          )}
+        </div>
+      </div>
     </div>
+
   );
 };
 
