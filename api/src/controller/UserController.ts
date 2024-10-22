79566ab2e66a82c9e2724d7abce193ec2318b81f
cwtshh <gucosta1719@gmail.com>
2024-10-22 07:59:52 -0300

    adicionando contexto ao chat e iniciando a função de envio de arquivo pelo chat

--- api/src/controller/UserController.ts
+++ api/src/controller/UserController.ts
@@ -8,6 +8,9 @@ import { query } from 'express-validator';
 import Message from '../model/Message';
 import { send_message_to_ai } from './MessageController';
 import { FlowiseClient } from 'flowise-sdk';
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
 
 const SECRET = process.env.SECRET_KEY || 'secret';
 
@@ -114,6 +117,82 @@ interface MessageBody {
 
 let url = "https://flowise.aidadpdf.cloud/api/v1/prediction/70873bc0-fd4d-4d77-9781-18178d0d38a6";
 
+
+
+const send_message_file = async(req: Request, res: Response) => {
+    const { chat_id, user_id } = req.body;
+    const file = req.file;
+
+    const upload_directory = path.join(__dirname, '..', '..', 'uploads');
+    console.log(upload_directory);
+    if(!fs.existsSync(upload_directory)) {
+        fs.mkdirSync(upload_directory);
+    }
+
+    const storage = multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, upload_directory);
+        },
+        filename: function(req, file, cb) {
+            cb(null, `${file.originalname}-${Date.now()}${path.extname(file.originalname)}`);
+        }
+    });
+
+    const client = new FlowiseClient({
+        baseUrl: 'https://flowise.aidadpdf.cloud',
+    })
+
+    const upload = multer({storage: storage}).single('file');
+    upload(req, res, async(err) => {
+        const { message } = req.body;
+        if(err) {
+            res.status(400).json({ errors: ['Erro ao enviar arquivo.'] });
+            return;
+        }
+        const file_ = req.file;
+
+        if (!file_) {
+            res.status(400).json({ errors: ['Arquivo não encontrado.'] });
+            return;
+        }
+
+        const fileData = fs.readFileSync(path.join(upload_directory, file_.filename)).toString('base64');
+        const file_object = {
+            type: 'file',
+                    name: file_.originalname,
+                    data: `data:${file_.mimetype};base64,${fileData}`,
+                    mime: file_.mimetype
+        }
+        console.log(message);
+        try {
+            const prediction = await client.createPrediction({
+                chatflowId: "6ac71f88-8509-4230-89f9-111e84669633",
+                question: message,
+                uploads: [{
+                    type: 'file',
+                    name: file_.originalname,
+                    data: `data:${file_.mimetype};base64,${fileData}`,
+                    mime: file_.mimetype,
+                    
+                }],
+            })
+            
+            
+
+            console.log(prediction);
+            res.status(201).json({ message: 'Arquivo enviado com sucesso.', ai_message: prediction.text });
+            return;
+        } catch (error) {
+            console.error(error);
+            res.status(400).json({ errors: ['Erro ao enviar arquivo.'] });
+            return;
+        }
+
+
+        // res.status(201).json({ message: 'Arquivo enviado com sucesso.', });
+    })
+};
+
 const send_message = async(req: Request, res: Response) => {
 
     const { message, chat_id, user_id } = req.body;
@@ -129,127 +208,149 @@ const send_message = async(req: Request, res: Response) => {
     
     const client = new FlowiseClient({
         baseUrl: 'https://flowise.aidadpdf.cloud',
+        apiKey: '4QyRe4cw5wKaxvIgNS6aWYQzoQeWv4j9OsYu4iGiwbY',
     });
 
-    const prediction = await client.createPrediction({
-        chatflowId: "70873bc0-fd4d-4d77-9781-18178d0d38a6",
-        question: message,
-    });
+    try {
+        if(chat.chat_sessionid === '') {
+            console.log('sem chat session id');
+            const prediction = await client.createPrediction({
+                chatflowId: "70873bc0-fd4d-4d77-9781-18178d0d38a6",
+                question: message,
+            });
+            chat.chat_sessionid = prediction.sessionId;
+            await chat.save();
 
-    if(chat.chat_sessionid === '') {
-        chat.chat_sessionid = prediction.sessionId;
-        await chat.save();
-    }
+            if(!prediction) {
+                res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
+                return;
+            }
 
-    if(!prediction) {
+            console.log(prediction);
+
+            const new_message = await Message.create({
+                content: message,
+                chat: chat_id,
+                sent_by: 'user',
+                user_id: user_id,
+            });
+
+            if(!new_message) {
+                res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
+                return;
+            }
+
+            const new_ai_message = await Message.create({
+                content: prediction.text || 'Erro ao enviar mensagem.',
+                chat: chat_id,
+                sent_by: 'assistant',
+                user_id: user_id,
+            });
+
+            if(!new_ai_message) {
+                res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
+                return;
+            }
+            chat.messages.push(new_message._id);
+            chat.messages.push(new_ai_message._id);
+            await chat.save();
+            res.status(201).json({ message: 'Mensagem enviada com sucesso.', ai_message: prediction.text || 'Erro ao enviar mensagem.' });
+            return;
+        }
+        if(chat.chat_sessionid !== '') {
+            console.log('tem session id');
+            const prediction = await client.createPrediction({
+                chatflowId: "70873bc0-fd4d-4d77-9781-18178d0d38a6",
+                question: message,
+                sessionId: chat.chat_sessionid
+            });
+
+            console.log(prediction);
+
+            if(!prediction) {
+                res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
+                return;
+            }
+
+            const new_message = await Message.create({
+                content: message,
+                chat: chat_id,
+                sent_by: 'user',
+                user_id: user_id,
+            });
+
+            if(!new_message) {
+                res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
+                return;
+            }
+
+            const new_ai_message = await Message.create({
+                content: prediction.text || 'Erro ao enviar mensagem.',
+                chat: chat_id,
+                sent_by: 'assistant',
+                user_id: user_id,
+            });
+
+            if(!new_ai_message) {
+                res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
+                return;
+            }
+            chat.messages.push(new_message._id);
+            chat.messages.push(new_ai_message._id);
+            await chat.save();
+            res.status(201).json({ message: 'Mensagem enviada com sucesso.', ai_message: prediction.text || 'Erro ao enviar mensagem.' });
+            return;
+        }
+    } catch (err) {
+        console.error(err);
         res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
         return;
     }
 
-    const new_message = await Message.create({
-        content: message,
-        chat: chat_id,
-        sent_by: 'user',
-        user_id: user_id,
-    });
-    if(!new_message) {
-        res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
-        return;
-    }
-    const new_ai_message = await Message.create({
-        content: prediction.text,
-        chat: chat_id,
-        sent_by: 'assistant',
-        user_id: user_id,
-    });
-    if(!new_ai_message) {
-        res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
-        return;
-    }
-
-    chat.messages.push(new_message._id);
-    chat.messages.push(new_ai_message._id);
-
-    await chat.save();
-
-    console.log(prediction);
-
-    res.status(201).json({ message: 'Mensagem enviada com sucesso.', ai_message: prediction.text });
-    // let variable = prediction.sourceDocuments[0].metadata.pdf.info.Title;
-    // console.log(variable);
-
-    // console.log(prediction);
-    // res.send(prediction);
-
-
-
-
-
-    // const { chat_id, user_id, message } = req.body;
-    // console.log(chat_id, user_id, message);
-    // const chat = await Chat.findById(chat_id).populate({
-    //     path: 'messages',
-    //     model: 'Message'
+    // const prediction = await client.createPrediction({
+    //     chatflowId: "70873bc0-fd4d-4d77-9781-18178d0d38a6",
+    //     question: message,
     // });
-    // if(!chat) {
-    //     res.status(400).json({ errors: ['Chat não encontrado.'] });
+
+    // if(chat.chat_sessionid === '') {
+    //     chat.chat_sessionid = prediction.sessionId;
+    //     await chat.save();
+    // }
+
+    // if(!prediction) {
+    //     res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
     //     return;
     // }
+
     // const new_message = await Message.create({
     //     content: message,
     //     chat: chat_id,
     //     sent_by: 'user',
-    //     user_id: user_id
+    //     user_id: user_id,
     // });
     // if(!new_message) {
     //     res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
     //     return;
     // }
-    
-    // chat.messages.push(new_message._id);
-    // let chat_history: MessageBody[] = chat.messages.map((message: any) => {
-    //     if (message.sent_by === 'user') {
-    //         const message_conv: MessageBody = {
-    //             role: 'user',
-    //             content: message.content
-    //         }
-    //         return message_conv;
-    //     }
-    //     if(message.sent_by === 'assistant') {
-    //         const message_conv: MessageBody = {
-    //             role: 'assistant',
-    //             content: message.content
-    //         }
-    //         return message_conv;
-    //     }
-    //     return undefined;
-    // }).filter((message): message is MessageBody => message !== undefined);
-    // const ai_message = await send_message_to_ai('llama3.1', chat_history, message);
-    // if (typeof ai_message === 'object' && ai_message !== null && 'response' in ai_message) {
-    //     // console.log((ai_message as { response: string }).response);
-    //     if(!ai_message) {
-    //         res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
-    //         return;
-    //     }
-    //     const ai_response = await Message.create({
-    //         content: (ai_message as { response: string }).response,
-    //         chat: chat_id,
-    //         sent_by: 'assistant',
-    //         user_id: user_id
-    //     });
-    //     if(!ai_response) {
-    //         res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
-    //         return;
-    //     }
-    //     chat.messages.push(ai_response._id);
-    //     await chat.save();
-    //     res.status(201).json({ message: 'Mensagem enviada com sucesso.', ai_message: (ai_message as { response: string }).response });
-    //     return;
-    // } else {
-    //     console.error('Unexpected AI message format:', ai_message);
+    // const new_ai_message = await Message.create({
+    //     content: prediction.text || 'Erro ao enviar mensagem.',
+    //     chat: chat_id,
+    //     sent_by: 'assistant',
+    //     user_id: user_id,
+    // });
+    // if(!new_ai_message) {
     //     res.status(400).json({ errors: ['Erro ao enviar mensagem.'] });
     //     return;
     // }
+
+    // chat.messages.push(new_message._id);
+    // chat.messages.push(new_ai_message._id);
+
+    // await chat.save();
+
+    // console.log(prediction);
+
+    // res.status(201).json({ message: 'Mensagem enviada com sucesso.', ai_message: prediction.text });
 };
 
 const get_chat = async(req: Request, res: Response) => {
@@ -273,4 +374,4 @@ const delete_chat = async(req: Request, res: Response) => {
 }
 
 
-export { register_user, login_user, logout_user, create_chat, get_all_user_chats, send_message, get_chat, delete_chat };
+export { register_user, login_user, logout_user, create_chat, get_all_user_chats, send_message, get_chat, delete_chat, send_message_file };
--- api/src/routes/UserRouter.ts
+++ api/src/routes/UserRouter.ts
@@ -1,9 +1,8 @@
 import express from 'express';
-import { create_chat, delete_chat, get_all_user_chats, get_chat, login_user, logout_user, register_user, send_message } from '../controller/UserController';
+import { create_chat, delete_chat, get_all_user_chats, get_chat, login_user, logout_user, register_user, send_message, send_message_file } from '../controller/UserController';
 import UserCreateValidations from '../middlewares/UserCreateValidation';
 import HandleValidations from '../middlewares/HandleValidations';
 import UserLoginValidations from '../middlewares/UserLoginValidation';
-
 
 const UserRouter = express();
 
@@ -12,13 +11,13 @@ UserRouter.post('/create', UserCreateValidations(), HandleValidations, register_
 UserRouter.post('/login', UserLoginValidations(), HandleValidations, login_user);
 UserRouter.post('/logout', logout_user);
 
-
 // chat
 UserRouter.post('/chat/create', create_chat);
 UserRouter.get('/chat/all/:id', get_all_user_chats);
 UserRouter.post('/chat/send', send_message);
 UserRouter.get('/chat/:chat_id', get_chat);
 UserRouter.delete('/chat/:chat_id', delete_chat);
+UserRouter.post('/chat/sendfile', send_message_file);
 
 
 export default UserRouter;
