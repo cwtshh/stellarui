a796bcbdb07f7c334de5273974936187ac3bd4fb
cwtshh <gucosta1719@gmail.com>
2024-10-22 10:17:37 -0300

    adicionando mensagem de erro

--- src/components/HighlightText/HighlightText.tsx
+++ src/components/HighlightText/HighlightText.tsx
@@ -1,3 +1,5 @@
+import { RiErrorWarningFill } from "react-icons/ri";
+
 interface HighlightTextProps {
     text: string;
 }
@@ -15,7 +17,16 @@ const HighlightText = ({ text }: HighlightTextProps) => {
 
                 // Se for o símbolo "-" isolado com espaços, aplica tabulação
                 if (part === ' - ') {
-                    return <span key={index} style={{ marginLeft: '20px' }}></span>; // Simula tabulação
+                    return <span key={index} style={{ marginLeft: '30px' }}></span>; // Simula tabulação
+                }
+
+                if (part.trim() === 'Erro ao enviar mensagem.') {
+                    return (
+                        <p className="flex items-center gap-2 text-red-500">
+                            <RiErrorWarningFill />
+                            {part}
+                        </p>
+                     ) // Aplica destaque
                 }
 
                 // Texto normal
